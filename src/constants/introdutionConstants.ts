86b305834f3dba6532a7b3ebe2481dcb0d7b7035
sunminnnnn <sslspplz@mju.ac.kr>
2024-09-30 00:19:35 +0900

    [feat] 디폴트 값 넣기2

---
+++ src/constants/introdutionConstants.ts
@@ -0,0 +1,21 @@
+export const CEO_DATA = {
+  CEO_Instruction:
+    'CJ ENM 디지털 제작 팀장 출신 \n TV 예능, 웹드라마, 디지털 다큐멘터리, 게임 콘텐츠 연출 \n 기업 및 정부기관 콘텐츠 제작 및 SNS 운영 \n 엔터테인먼트 아티스트 콘텐츠 제작',
+  CEO_Name: '박성진',
+};
+
+export const COMPANY_DATA = {
+  Address: '서울시 성동구 광나루로 162 BS성수타워 5층',
+  English_Address: '5F 162, Gwangnaru-ro, Seongdong-gu, Seoul, \n Republic of Korea',
+  Number: '02-2038-2663',
+  Introduction1: '스튜디오 아이와 함께  영상물 퀄리티 UP',
+  Introduction2: '최고의 경험을 선사하는 스튜디오 아이의 작업과 함께하세요.',
+  Introduction3: '스튜디오 아이는 편집 및 애니메이팅을 하고 있는 영상 매체 작업 전문 기업입니다.',
+  Detail1: '그간의 작업물을 쉽게 둘러보세요.',
+  Detail2: '사이트를 통해 간편하게 문의하세요.',
+};
+
+export const PARTER_DATA = {
+  PLADI_LINK: 'https://www.pladi.tv/',
+  LOCOMO_LINK: 'https://www.youtube.com/channel/UCDwpuTfjBB8ZOmbnE',
+};
