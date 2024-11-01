dda7b912325e26ed62be712dc37f6ecbae238c86
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-11-01 21:21:48 +0900

    feat: test를 위한 alert, confirm prompt hook 생성

--- cypress/support/hooks.ts
+++ cypress/support/hooks.ts
@@ -1,7 +1,25 @@
-export const login=()=>{
+import { MSG } from '@/constants/messages';
+
+export const login = () => {
   cy.visit('/login');
   cy.get('#id').type('master');
   cy.get('#pw').type('master');
   cy.contains('로그인').click();
   cy.url().should('include', '/promotion-admin/home');
-}
+};
+
+// 메시지를 매개변수로 받아 사용하는 범용 훅
+export const confirmAndCheckCompletion = (confirmMessage: string, alertMessage: string) => {
+  cy.on('window:confirm', (text) => {
+    // Cypress의 expect 사용
+    cy.wrap(text).should('equal', confirmMessage); // 확인 창 메시지 확인
+    return true; // 확인 선택
+  });
+
+  cy.contains(alertMessage).should('be.visible'); // 완료 메시지 확인
+  cy.on('window:alert', (text) => {
+    // Cypress의 expect 사용
+    cy.wrap(text).should('equal', alertMessage); // 알림 창 메시지 확인
+    return true; // 알림 확인
+  });
+};
