52618bd61e6c44d28fffa2bc66bbb398826c59dc
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-11-25 16:40:04 +0900

    chore: loading 페이지 폴더 이동

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Loading = () => {
+  return <Container></Container>;
+};
+
+export default Loading;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100svh;
+  background-color: black;
+  transition: all 300ms ease-in-out;
+`;
